bf26bfc02e03872b8e5e0c80f5232f22d6d90960
diegoror1 <diegorordriguez159@gmail.com>
2022-11-09 11:16:49 -0400

    ya recibiendo datos y mostrandolos en graficas

--- pyth/graf.py
+++ pyth/graf.py
@@ -6,18 +6,18 @@ arduinoSerialData = serial.Serial("COM3", 9600 , timeout=1)
 
 fig = plt.figure()
 ax = fig.add_subplot(1,1,1)
-xdatos, ydatos = [], []
+xdatos = []
+ydatos = []
 
 while True:
-    if (arduinoSerialData()>0):
+    if (arduinoSerialData.inWaiting()>0):
         def animate(i,xdatos,ydatos):
             datos = arduinoSerialData.readline()
             datos = float (datos)
-            xdatos.appened(i)
-            ydatos.appener(datos)
+            print(datos)
+            xdatos.append(i)
+            ydatos.append(datos)
             ax.clear()
-            ax.plot()
             ax.plot(xdatos,ydatos)
-
         ani = animation.FuncAnimation(fig, animate, fargs = (xdatos,ydatos))
         plt.show()
